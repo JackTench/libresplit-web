dc17f67389b65d571741b5a2a62dd1443200cc1f
JackTench <79285604+JackTench@users.noreply.github.com>
2025-06-03 19:49:12 +0100

    Add animation to sub-heading on landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { PointerHighlight } from "@/components/ui/pointer-highlight";
+
 export default function HomePage() {
   return (
     <div className="flex h-20 flex-col items-center justify-center">
       <h1 className="text-7xl">LibreSplit</h1>
-      <h2 className="h-5 text-xl">
-        Free speedrun timer with auto splitting and load removal for Linux.
-      </h2>
+      <div className="h-5 text-xl">
+        Free speedrun timer with auto splitting and load removal
+        <PointerHighlight>for Linux.</PointerHighlight>
+      </div>
     </div>
   );
 }
