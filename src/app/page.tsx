47d27675d2695c56900d9d1e16c9d05fc42f5de1
JackTench <79285604+JackTench@users.noreply.github.com>
2025-06-03 19:41:40 +0100

    Redesign landing page.
    Dark/Light toggle is missing for now. It will return elsewhere in the
    UI.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { AppThemeToggleButton } from "@/components/libresplit-web/AppThemeToggleButton";
-
 export default function HomePage() {
   return (
-    <div>
-      <AppThemeToggleButton />
-      LibreSplit
+    <div className="flex h-20 flex-col items-center justify-center">
+      <h1 className="text-7xl">LibreSplit</h1>
+      <h2 className="h-5 text-xl">
+        Free speedrun timer with auto splitting and load removal for Linux.
+      </h2>
     </div>
   );
 }
