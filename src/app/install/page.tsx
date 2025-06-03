399f6e1f6be0efff2d2ac7dbb0760e9377223f8e
JackTench <79285604+JackTench@users.noreply.github.com>
2025-06-03 21:17:52 +0100

    First iteration of install page.

---
+++ src/app/install/page.tsx
@@ -0,0 +1,25 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+
+export default function InstallPage() {
+  return (
+    <Accordion type="single" collapsible defaultValue="item-1" className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Build From Source</AccordionTrigger>
+        <AccordionContent>
+          <p>
+            Follow the below instructions to build LibreSplit from the source code.
+          </p>
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Arch Linux</AccordionTrigger>
+        <AccordionContent className="flex flex-col gap-4 text-balance">
+          <p>
+            LibreSplit is available in the AUR and can either be installed with an AUR helper, or manually.
+          </p>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+}
