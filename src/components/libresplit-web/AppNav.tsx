524a169882c8e714a513c7371d866ac743c11668
JackTench <79285604+JackTench@users.noreply.github.com>
2025-06-03 18:36:37 +0100

    Create left part of nav.

--- src/components/libresplit-web/AppNav.tsx
+++ src/components/libresplit-web/AppNav.tsx
@@ -1,5 +1,37 @@
 "use client";
 
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import Image from "next/image";
+
 export function AppNav() {
-  return <h1>Placeholder Text</h1>;
+  return (
+    <nav>
+      <NavigationMenu>
+        <NavigationMenuList>
+          {/* Home Button - Logo and Text */}
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link href="/">
+                <div className="flex items-center gap-2">
+                  <Image
+                    src="logo.svg"
+                    width={32}
+                    height={32}
+                    alt="LibreSplit"
+                  />
+                  <span className="text-xl">LibreSplit</span>
+                </div>
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </nav>
+  );
 }
