3a00fd861ab21033293e2f120e4a5d95b1a153d4
JackTench <79285604+JackTench@users.noreply.github.com>
2025-06-03 19:25:44 +0100

    Create custom loading page.

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import { BarLoader } from "react-spinners";
+
+export default function Loading() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center space-y-4">
+      <BarLoader color="#3F83F8" />
+      <p>Not found.</p>
+    </div>
+  );
+}
