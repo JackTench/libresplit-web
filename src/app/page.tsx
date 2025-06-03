a30e57cc123a1c6d9364be108c6534def7b8912b
JackTench <79285604+JackTench@users.noreply.github.com>
2025-06-03 20:21:01 +0100

    Delay "for Linux" animation on landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 import { PointerHighlight } from "@/components/ui/pointer-highlight";
 
 export default function HomePage() {
+  // Fire "For Linux" animation after 3 seconds.
+  const [showForLinuxAnim, setShowForLinuxAnim] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowForLinuxAnim(true);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="flex h-20 flex-col items-center justify-center">
       <h1 className="text-7xl">LibreSplit</h1>
       <div className="h-5 text-xl">
         Free speedrun timer with auto splitting and load removal
-        <PointerHighlight>for Linux.</PointerHighlight>
+        {showForLinuxAnim ? <ForLinuxTextAnimated /> : <ForLinuxTextPlain />}
       </div>
     </div>
   );
+}
+
+function ForLinuxTextPlain() {
+  return <p>for Linux.</p>;
+}
+
+function ForLinuxTextAnimated() {
+  return <PointerHighlight>for Linux.</PointerHighlight>;
 }
