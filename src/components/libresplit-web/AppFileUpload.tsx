9d80f3985dc935a9035f435543db13838d249faa
JackTench <79285604+JackTench@users.noreply.github.com>
2025-06-04 18:45:57 +0100

    Only enable button when file is selected.

--- src/components/libresplit-web/AppFileUpload.tsx
+++ src/components/libresplit-web/AppFileUpload.tsx
@@ -1,11 +1,21 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 export function AppFileUpload() {
+  // Only enable button when file is selected.
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0] || null;
+    setSelectedFile(file);
+  };
+
   return (
     <div>
-      <Input id="file" type ="file" />
-      <Button>Convert</Button>
+      <Input id="file" type="file" onChange={handleFileChange} />
+      <Button disabled={!selectedFile}>Convert</Button>
     </div>
   );
 }
